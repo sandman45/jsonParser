d151925607cbc68cb48f66d93de545eabafa257c
sandman45 <msanders45@gmail.com>
2018-08-16 17:00:17 -0600

    read write json

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const fs = require('fs');
+const fileManipulator = require('./modules/fileManipulation');
 
-module.exports = {
-    executeFlow: () => {
+const absPath = `${__dirname}/files/`;
+let fileData;
 
-    },
-};
+fileManipulator.readFile('jsonFile.json', absPath).then((data) => {
+    const dataObject = JSON.parse(data);
+    dataObject.movies.push({
+        title: 'Top Gun',
+    });
+    fileData = JSON.stringify(dataObject);
+
+    return fileManipulator.writeFile('jsonFile.json', absPath, fileData).then((results) => {
+        console.log(results);
+        return results;
+    });
+}).catch((error) => {
+    console.log(error);
+});
